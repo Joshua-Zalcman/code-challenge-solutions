9de70a8f1dfdd01b61f390fd5a2a5a164e3d247e
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-08-19 16:45:37 -0400

    adds challenge

--- problems.js
+++ problems.js
@@ -238,3 +238,19 @@ function rounders(n) {
     }
     return +arr.reverse().join("")
 }
+//============================================
+// 12)When a candle finishes burning it leaves a leftover. makeNew leftovers can be combined to make a new candle, which, when burning down, will in turn leave another leftover.
+
+// You have candlesNumber candles in your possession. What's the total number of candles you can burn, assuming that you create new candles as soon as you have enough leftovers?
+
+//solution:
+function candles(candlesNumber, makeNew) {
+    let count = candlesNumber;
+    let leftover = candlesNumber;
+    while(leftover >= makeNew) {
+        leftover -= makeNew;
+        count++
+        leftover++
+    }
+    return count
+}
