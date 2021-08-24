9f8ad1cd2afeac71bf7a693a273183a9c3891f0c
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-08-23 21:15:00 -0400

    adds challenge

--- problems.js
+++ problems.js
@@ -254,10 +254,30 @@ function candles(candlesNumber, makeNew) {
     }
     return count
 }
+//============================================
 
 // 13)Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
 
 //solution:
 function arrayReplace(inputArray, elemToReplace, substitutionElem) {
     return inputArray.map(x=> x === elemToReplace ? substitutionElem : x)
+}
+//============================================
+
+// 14)We define the middle of the array arr as follows:
+
+// if arr contains an odd number of elements, its middle is the element whose index number is the same when counting from the beginning of the array and from its end;
+// if arr contains an even number of elements, its middle is the sum of the two elements whose index numbers when counting from the beginning and from the end of the array differ by one.
+// An array is called smooth if its first and its last elements are equal to one another and to the middle. Given an array arr, determine if it is smooth or not.
+
+//solution:
+function isSmooth(arr) {
+    let middle
+    if(arr.length%2 !== 0){
+        middle = arr[Math.floor(arr.length/2)]
+    } else {
+        middle = arr[arr.length/2] + arr[(arr.length/2)-1]
+    }
+    console.log(middle)
+    return arr[0] === arr[arr.length-1] && arr[0] === middle
 }
