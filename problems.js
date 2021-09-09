184df7fdb4a8a787298c84bc82be4cdc320b0a46
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-09-08 21:31:43 -0400

    adds challenge

--- problems.js
+++ problems.js
@@ -353,3 +353,16 @@ function weakNumbers(n) {
     console.log(weakArr)
     return [weakest, weakArr.filter(x=> x===weakest).length]
 }
+//============================================
+
+// 19)You are implementing your own HTML editor. To make it more comfortable for developers you would like to add an auto-completion feature to it.
+
+// Given the starting HTML tag, find the appropriate end tag which your editor should propose.
+
+// If you are not familiar with HTML, consult with this note.
+
+//solution:
+function htmlEndTagByStartTag(startTag) {
+    let tag = startTag.split(' ')[0].replace('<','')
+    return tag.includes('>') ? `</${tag}`:`</${tag}>`
+}
