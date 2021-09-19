a7d6b92712a2212e0cbfcd545292327328545f8d
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-09-19 18:58:55 -0400

    adds challenge

--- problems.js
+++ problems.js
@@ -457,3 +457,19 @@ function growingPlant(upSpeed, downSpeed, desiredHeight) {
         }
     }
 }
+//============================================
+
+// 24)Let's define digit degree of some positive integer as the number of times we need to replace this number with the sum of its digits until we get to a one digit number.
+
+// Given an integer, find its digit degree.
+
+function digitDegree(n) {
+    let count = 0
+    while(n >= 10){
+        n = n.toString().split('').reduce((ac,num)=> ac +(+num),0)
+        count++
+    }
+    return count
+}
+
+//solution:
