7c30fba24b1d20669d1216bdd3c7476fca8e7c8f
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-09-28 16:55:27 -0400

    adds challeng

--- problems.js
+++ problems.js
@@ -589,3 +589,9 @@ function deleteDigit(n) {
     }
      return max
 }
+// 33)Define a word as a sequence of consecutive English letters. Find the longest word from the given string.
+
+//solution:
+function longestWord(text) {
+    return text.replace(/(\W|_)/g," ").split(" ").sort((a,b)=> b.length-a.length)[0]
+ }
