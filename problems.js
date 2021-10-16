91733e9c52f0e3c6f2b00b84fb1b74b0849f68b9
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-10-15 20:42:25 -0400

    adds challenge

--- problems.js
+++ problems.js
@@ -898,3 +898,20 @@ function reflectString(str) {
     }
     return arr.join('')
 }
+// 53)Your Informatics teacher at school likes coming up with new ways to help you understand the material. When you started studying numeral systems, he introduced his own numeral system, which he's convinced will help clarify things. His numeral system has base 26, and its digits are represented by English capital letters - A for 0, B for 1, and so on.
+
+// The teacher assigned you the following numeral system exercise: given a one-digit number, you should find all unordered pairs of one-digit numbers whose values add up to the number.
+
+//solution:
+function newNumeralSystem(number) {
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+    let arr = []
+    let num = alphabet.indexOf(number)
+    let count = 0
+    while(count <= num ){
+        arr.push(`${alphabet[count]} + ${alphabet[num]}`)
+        num--
+        count++
+    }
+    return arr
+}
