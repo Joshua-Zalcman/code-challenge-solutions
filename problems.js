24c8d3a36b5207fa04141eaffd5dd47f8772d858
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-10-17 16:54:35 -0400

    adds challenge

--- problems.js
+++ problems.js
@@ -922,3 +922,14 @@ function characterParity(symbol) {
     if(Number.isNaN(+symbol)) return 'not a digit'
     return +symbol % 2 === 0 ? 'even' : 'odd'
 }
+// 55)When you recently visited your little nephew, he told you a sad story: there's a bully at school who steals his lunch every day, and locks it away in his locker. He also leaves a note with a strange, coded message. Your nephew gave you one of the notes in hope that you can decipher it for him. And you did: it looks like all the digits in it are replaced with letters and vice versa. Digit 0 is replaced with 'a', 1 is replaced with 'b' and so on, with digit 9 replaced by 'j'.
+
+// The note is different every day, so you decide to write a function that will decipher it for your nephew on an ongoing basis.
+
+
+
+//solution:
+function stolenLunch(note) {
+    let alphabet = 'abcdefghij'.split('')
+    return note.split('').map(x=> /[a-j]/.test(x) ? alphabet.indexOf(x) : /[0-9]/.test(x) ? alphabet[+x] : x).join('')
+}
