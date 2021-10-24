fd462c88a32b125464ca5659ea8c73a556a54b27
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-10-24 13:53:14 -0400

    adds challenge

--- problems.js
+++ problems.js
@@ -1021,3 +1021,9 @@ function pairOfShoes(shoes) {
     }
     return true
 }
+// 60)Given a rectangular matrix and an integer column, return an array containing the elements of the columnth column of the given matrix (the leftmost column is the 0th one).
+
+//solution:
+function extractMatrixColumn(matrix, column) {
+    return matrix.map(x=> x[column])
+}
