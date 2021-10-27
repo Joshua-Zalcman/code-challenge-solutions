a6e0dd6e02c11e67e0a5fceee58ffe474c4963b6
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-10-27 15:57:07 -0400

    adds challenge

--- problems.js
+++ problems.js
@@ -1059,3 +1059,32 @@ function areIsomorphic(array1, array2) {
     }
     return true
 }
+// 63)You are watching a volleyball tournament, but you missed the beginning of the very first game of your favorite team. Now you're curious about how the coach arranged the players on the field at the start of the game.
+
+// The team you favor plays in the following formation:
+
+// 0 3 0
+// 4 0 2
+// 0 6 0
+// 5 0 1
+// where positive numbers represent positions occupied by players. After the team gains the serve, its members rotate one position in a clockwise direction, so the player in position 2 moves to position 1, the player in position 3 moves to position 2, and so on, with the player in position 1 moving to position 6.
+
+// Given the current formation of the team and the number of times k it gained the serve, find the initial position of each player in it.
+
+//solution:
+function volleyballPositions(frm, k) {
+    k = k%6
+    let players=[frm[3][2],frm[1][2],frm[0][1],frm[1][0],frm[3][0],frm[2][1]]
+    while(k>0){
+        let player = players.pop()
+        players.unshift(player)
+        k--
+    }
+    frm[3][2]= players[0]
+    frm[1][2]= players[1]
+    frm[0][1]= players[2]
+    frm[1][0]= players[3]
+    frm[3][0]= players[4]
+    frm[2][1]= players[5]
+    return frm
+}
